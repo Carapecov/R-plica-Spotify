f3abbb69f229a15725f1a1c23fc03206037ccfac
Giovane <gabriel12zlc@gmail.com>
2025-02-24 21:29:02 -0300

    Busca total por Id

--- src/pages/Song.jsx
+++ src/pages/Song.jsx
@@ -1,26 +1,40 @@
 import React from 'react'
 import Player from '../components/Player'
-import { Link } from 'react-router-dom'
+import { Link, useParams} from 'react-router-dom'
+import { songsArray } from '../assets/database/songs'
+import { artistArray } from '../assets/database/artists'
 const Song = () => {
+  const { id } = useParams();
+
+
+  const { image, name, duration, artist, audio} =
+    songsArray.filter((currentSongObj) => currentSongObj.id === Number(id))
+    [0];
+
+    const artistObj =
+    artistArray.filter((currentArtistObj) => currentArtistObj.name === artist)
+    [0];
+
+
   return (
     <div className='song'>
       <div className="song__container">
         <div className="song__image-container">
-        <img src="https://i.scdn.co/image/ab67616d00001e025645e61562deebdda78755b6" alt="Imagem da Musica X" /> 
+        <img src={image} alt={`Imagem da música ${name}`} /> 
         </div>
       </div>
 
       <div className="song__bar">
-        <Link to="/artist/1"  className='song__artist-image'>
+        <Link to={`/artist/${artistObj.id}`}  className='song__artist-image'>
           <img  width={75}
-          height={75} src="https://i.scdn.co/image/ab676161000051747ffc3038ef5e67354ecdede1" alt="Imagem do Artista Y" />
+          height={75} src={artistObj.image} alt={`Imagem do Artista ${artist}`} />
         </Link>
 
-        <Player />
+        <Player duration={duration} />
 
         <div>
-          <p className='song__name'>Coração Partido (Corazón Partío) - Ao Vivo</p>
-          <p>Grupo Menos É Mais</p>
+          <p className='song__name'>{name}</p>
+          <p>{artist}</p>
         </div>
       </div>
 
