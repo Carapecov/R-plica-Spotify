fc7a7fd34ca67ed504b9e045b38f7efec62c22f5
Giovane <gabriel12zlc@gmail.com>
2025-02-27 00:05:18 -0300

    Criação de API e conexão Mongo

---
+++ back-end/api/insertMany.js
@@ -0,0 +1,9 @@
+import { artistArray } from "../../src/assets/database/artists.js"
+import { songsArray } from "../../src/assets/database/songs.js"
+
+const newArtistArray = artistArray.map((currentArtistObj) => {
+    const newArtistObj = {...currentArtistObj}
+})
+
+console.log(artistArray)
+console.log(songsArray)
---
+++ back-end/api/server.js
@@ -0,0 +1,22 @@
+import express from 'express';
+import { db } from "./connect.js"
+
+
+const app = express();
+const port = 3000;
+
+app.get('/', (request, response) => {
+  response.send("Hello World!");
+})
+
+app.get('/artists', async(request, response) => {
+    response.send(await db.collection("artists").find({}).toArray());
+  })
+
+app.get('/songs', async(request, response) => {
+    response.send(await db.collection("songs").find({}).toArray());
+  })
+
+app.listen(port, () => {
+    console.log(`Server is running on port ${port}`);
+    });
